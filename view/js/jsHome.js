1a6c29b2a8a4c8ebe11fe558fc808842534a52d2
Amanda <amandaivoniak@gmail.com>
2019-11-13 22:24:26 -0200

    Fix login

--- view/js/jsHome.js
+++ view/js/jsHome.js
@@ -6,15 +6,13 @@ $(document).ready(function () {
         data: {},
         success: function (data) {
            var data =  JSON.parse(data);
-
+           console.log(data);
            for (let i = 0; i < data.length; i++) {
-            montaCards(data[i].titulo, data[i].descricao)             
+            montaCards(data[i].titulo, data[i].descricao);          
            }
         }
       });
 });
-
-
 
 function menu(menu) {
     if (menu === 'home') {
@@ -27,22 +25,22 @@ function menu(menu) {
 }
 
 function montaCards(title, description){
-var address = 'blablabla ';
-var cardFlow = document.getElementById("cardFlow");
-cardFlow.innerHTML += '<div class="col s12 m3">'+
-                        '<div class="card">'+
-                           '<div class="card-image waves-effect waves-block waves-light">'+
-                              '<img class="activator" src="images/0.jpg">'+
-                           '</div>'+
-                           '<div class="card-content">'+
-                              '<span class="card-title activator grey-text text-darken-4">'+ 
-                              title +'<i class="material-icons right">more_vert</i></span>'+
-                              '<p><a href="'+address+'">Acessar Fluxo</a></p>'+
-                           '</div>'+
-                           '<div class="card-reveal">'+
-                              '<span class="card-title grey-text text-darken-4">'+ title +'<i class="material-icons right">close</i></span>'+
-                              '<p>'+ description +'</p>'+
-                           '</div>'+
-                        '</div>'+
-                      '</div>';
-                    }
+    var address = 'blablabla ';
+    var cardFlow = document.getElementById("cardFlow");
+    cardFlow.innerHTML += '<div class="col s12 m3">'+
+                            '<div class="card">'+
+                            '<div class="card-image waves-effect waves-block waves-light">'+
+                                '<img class="activator" src="images/0.jpg">'+
+                            '</div>'+
+                            '<div class="card-content">'+
+                                '<span class="card-title activator grey-text text-darken-4">'+ 
+                                title +'<i class="material-icons right">more_vert</i></span>'+
+                                '<p><a href="'+address+'">Acessar Fluxo</a></p>'+
+                            '</div>'+
+                            '<div class="card-reveal">'+
+                                '<span class="card-title grey-text text-darken-4">'+ title +'<i class="material-icons right">close</i></span>'+
+                                '<p>'+ description +'</p>'+
+                            '</div>'+
+                            '</div>'+
+                        '</div>';
+}
